fa6ae77a48980434579ec924bb8cfe4094c30877
Thiago Guardado Beltran <beltran.thiago@gmail.com>
2018-06-26 01:08:52 -0300

    Unicorn enemy

---
+++ Assets/Scripts/Enemies/Unicorn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unicorn : Enemy {
+
+    public Transform scoreAnchor;
+
+
+	protected override void Kill()
+	{
+        // add score to game controller
+        GetComponent<ObjectScore>().IncrementGameScore(scoreAnchor.position);
+
+        // kill object
+        Destroy(gameObject);
+	}
+
+}
